879ee498ea3461f81e7ef353786df4d38e406440
leon segal <leonsegal1@gmail.com>
2021-11-26 16:01:39 +0000

    add draft solution

--- src/resources/item/__tests__/item.router.spec.js
+++ src/resources/item/__tests__/item.router.spec.js
@@ -4,16 +4,17 @@ describe('item router', () => {
   test('has crud routes', () => {
     const routes = [
       { path: '/', method: 'get' },
+      { path: '/', method: 'post' },
       { path: '/:id', method: 'get' },
       { path: '/:id', method: 'delete' },
-      { path: '/:id', method: 'put' },
-      { path: '/', method: 'post' }
+      { path: '/:id', method: 'put' }
     ]
 
     routes.forEach(route => {
       const match = router.stack.find(
-        s => s.route.path === route.path && s.route.methods[route.method]
+        elem => elem.route.path === route.path && elem.route.methods[route.method]
       )
+
       expect(match).toBeTruthy()
     })
   })
--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ import cors from 'cors'
 import { connect } from './utils/db'
 
 export const app = express()
-
 app.disable('x-powered-by')
 
 app.use(cors())
@@ -14,12 +13,33 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = async () => {
+const router = express.Router()
+
+router
+  .route('/')
+  .get((_req, res) => res.send('hello get'))
+  .post((_req, res) => res.send('hello post'))
+
+router
+  .route('/:id')
+  .get((_req, res) => res.send('hello get id'))
+  .put((_req, res) => res.send('hello put id'))
+  .delete((_req, res) => res.send('hello delete id'))
+
+app.use(router)
+
+app.get('/api/routes', (req, res) => {
+  res.send(router.stack.find(
+    elem => elem.route.path === '/:id' && elem.route.methods['delete']
+  ))
+})
+export async function start() {
   try {
     await connect()
-    app.listen(config.port, () => {
+
+    app.listen(config.port, () =>
       console.log(`REST API on http://localhost:${config.port}/api`)
-    })
+    )
   } catch (e) {
     console.error(e)
   }
